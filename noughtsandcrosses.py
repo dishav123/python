00481165ec459b9de34baf50354435f161136402
dishav123 <155233720+dishav123@users.noreply.github.com>
2024-02-06 22:59:33 +0545

    Updnoughtsandcrosses.py

--- noughtsandcrosses.py
+++ noughtsandcrosses.py
@@ -89,10 +89,8 @@ def choose_computer_move(board):
         choose=random.choice(empty_box)
         row=choose[0]
         col=choose[1]
-        return row,col
+        return row, col
     
-
-
 def check_for_win(board, mark):
     # develop code to check if either the player or the computer has won
     # return True if someone won, False otherwise
@@ -213,22 +211,7 @@ def display_leaderboard(leaders):
         print(f'Hey {key}!, your highest score is {value}.')
     pass
 
-board = [ ['1','2','3'],\
-        ['4','5','6'],\
-        ['7','8','9']]
-welcome(board)
-total_score = 0
-while True:
-    choice = menu()
-    if choice == '1':
-        score = play_game(board)
-        total_score += score
-        print('Your current score is:',total_score)
-    if choice == '2':
-        save_score(total_score)
-    if choice == '3':
-        leader_board = load_scores()
-        display_leaderboard(leader_board)
-    if choice == 'q':
-        print('Thank you for playing the "Unbeatable Noughts and Crosses" game.')
-        print('Good bye')
+
+
+
+
